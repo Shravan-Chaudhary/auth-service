05e908ad912165f96024d90877e5940d05f4ab43
Shravan Chaudhary <bitznatch1110@gmail.com>
2023-12-02 13:40:47 +0530

    generate access token

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,9 +1,13 @@
+import fs from "fs";
 import { NextFunction, Response } from "express";
+import { JwtPayload, sign } from "jsonwebtoken";
 import { RegisterUserRequest } from "../types";
 import { UserService } from "../services/UserService";
 import { User } from "../entity/User";
 import { Logger } from "winston";
 import { validationResult } from "express-validator";
+import path from "path";
+import createHttpError from "http-errors";
 
 export class AuthController {
   constructor(
@@ -30,6 +34,33 @@ export class AuthController {
         password,
       });
       this.logger.info("User registered", { id: user.id });
+
+      // generate JWT token
+      let privateKey: Buffer;
+      try {
+        privateKey = fs.readFileSync(path.join(__dirname, "../../certs/private.pem"));
+      } catch (err) {
+        const error = createHttpError(500, "Error while reading private key");
+        next(error);
+        return;
+      }
+      const payload: JwtPayload = {
+        sub: String(user.id),
+        role: user.role,
+      };
+      const accessToken = sign(payload, privateKey, {
+        algorithm: "RS256",
+        expiresIn: "1h",
+        issuer: "auth-service",
+      });
+
+      res.cookie("accessToken", accessToken, {
+        domain: "localhost",
+        sameSite: "strict",
+        maxAge: 1000 * 60 * 60, // 1 hour
+        httpOnly: true,
+      });
+
       res.status(201).json({ id: user.id });
     } catch (err) {
       next(err);
