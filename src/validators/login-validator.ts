97ed28535005af1fdbc4f6f65a71855f3a6b9040
Shravan Chaudhary <bitznatch1110@gmail.com>
2023-12-06 15:06:06 +0530

    feat: add login validator and update register validator

--- src/validators/login-validator.ts
+++ src/validators/login-validator.ts
@@ -9,17 +9,6 @@ export default checkSchema({
       errorMessage: "Invalid Email",
     },
   },
-  firstName: {
-    errorMessage: "First name is required",
-    notEmpty: true,
-    trim: true,
-  },
-  lastName: {
-    errorMessage: "Last name is required",
-    notEmpty: true,
-    trim: true,
-  },
-
   password: {
     errorMessage: "Password is required",
     notEmpty: true,
