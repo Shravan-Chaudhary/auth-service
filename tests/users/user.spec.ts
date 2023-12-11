615626443376a006df677277f4c376c065491ef6
Shravan <shravansinghchaudhary@gmail.com>
2023-12-11 20:22:42 +0530

    test: add access token to GET /auth/self request test

--- tests/users/user.spec.ts
+++ tests/users/user.spec.ts
@@ -31,7 +31,16 @@ describe("GET /auth/self", () => {
 
   describe("Given all fields", () => {
     it("should return 200 status code", async () => {
-      const response = await request(app).get("/auth/self").send();
+      // generate token
+      const accessToken = jwks.token({
+        sub: "6",
+        role: Roles.CUSTOMER,
+      });
+
+      const response = await request(app)
+        .get("/auth/self")
+        .set("Cookie", [`accessToken=${accessToken};`])
+        .send();
       expect(response.statusCode).toBe(200);
     });
 
