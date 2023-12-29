74a3ab58a7b8f9140a0113e9714a84210cb2b1ed
Shravan <shravansinghchaudhary@gmail.com>
2023-12-29 22:16:47 +0530

    feat: add tenantController class with create method

---
+++ src/controllers/TenantController.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Response } from "express";
+import { TenantService } from "../services/TenantService";
+import { CreateTenantRequest } from "../types";
+import { Logger } from "winston";
+
+export class TenantController {
+  constructor(
+    private tenantService: TenantService,
+    private logger: Logger
+  ) {}
+  async create(req: CreateTenantRequest, res: Response, next: NextFunction) {
+    // validation and sanitization of request body
+
+    const { name, address } = req.body;
+    this.logger.debug("New Request to create a tenant", { name, address });
+
+    try {
+      const tenant = await this.tenantService.create({ name, address });
+      this.logger.info("Tenant created", { id: tenant.id });
+      res.status(201).json({ id: tenant.id });
+    } catch (err) {
+      next(err);
+      return;
+    }
+  }
+}
