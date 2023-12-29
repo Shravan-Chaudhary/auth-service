1101f8bd0e802184e9201366105d770f0b505dbb
Shravan <shravansinghchaudhary@gmail.com>
2023-12-29 22:15:22 +0530

    feat: add tenantService class for creating tenants

---
+++ src/services/TenantService.ts
@@ -0,0 +1,36 @@
+import { Repository } from "typeorm";
+import { TenantData } from "../types";
+import { Tenant } from "../entity/Tenant";
+import createHttpError from "http-errors";
+import { Roles } from "../constants";
+
+export class TenantService {
+  constructor(private tenantRepository: Repository<Tenant>) {}
+
+  async create({ name, address }: TenantData): Promise<Tenant> {
+    // Check if tenant already exists
+    const tenant = await this.tenantRepository.findOne({
+      where: {
+        name: name,
+      },
+    });
+
+    if (tenant) {
+      if (tenant) {
+        const error = createHttpError(409, "Tenant already exists with this name");
+        throw error;
+      }
+    }
+    // Store tenant in database
+    try {
+      return await this.tenantRepository.save({
+        name,
+        address,
+        role: Roles.ADMIN,
+      });
+    } catch (err) {
+      const error = createHttpError(500, "Failed to store tenant in database");
+      throw error;
+    }
+  }
+}
