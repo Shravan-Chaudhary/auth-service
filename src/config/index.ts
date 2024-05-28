195e5b86a8d5eb7569ae27dec1ac31f996d45b4c
Shravan-Chaudhary <shravansinghchaudhary@gmail.com>
2024-05-28 06:42:58 +0530

    refactor: add dotenv package and update server configuration

---
+++ src/config/index.ts
@@ -0,0 +1,10 @@
+import { config } from "dotenv";
+
+config();
+
+const { NODE_ENV, PORT } = process.env;
+
+export const Config = {
+    NODE_ENV,
+    PORT,
+};
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import app from "./app";
+import { Config } from "./config";
 import logger from "./config/logger";
 
 const startServer = () => {
-    const PORT = 5502;
+    const PORT = Config.PORT || 5502;
     try {
         app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
     } catch (err: unknown) {
