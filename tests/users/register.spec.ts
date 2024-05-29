16115f82ac00c1207881dc1a414eff70e1a04105
Shravan-Chaudhary <shravansinghchaudhary@gmail.com>
2024-05-29 14:09:41 +0530

    test: :test_tube: add user registration tests and database integration

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,7 +1,28 @@
 import request from "supertest";
 import app from "../../src/app";
+import { DataSource } from "typeorm";
+import AppDataSourse from "../../src/config/data-source";
+import { User } from "../../src/entity/User";
 
 describe("POST /auth/register", () => {
+    let connection: DataSource;
+
+    beforeAll(async () => {
+        // Initialize the database connection
+        connection = await AppDataSourse.initialize();
+    });
+
+    beforeEach(async () => {
+        // Clear the database before each test
+        await connection.dropDatabase();
+        // await connection.synchronize();
+    });
+
+    afterAll(async () => {
+        // Close the database connection
+        await connection.destroy();
+    });
+
     // Happy Path
     describe("Given all fields", () => {
         it("should return 201 status code", async () => {
@@ -14,7 +35,9 @@ describe("POST /auth/register", () => {
             };
 
             // Act
-            const response = await request(app).post("/auth/register");
+            const response = await request(app)
+                .post("/auth/register")
+                .send(user);
 
             // Assert
             expect(response.status).toBe(201);
@@ -30,12 +53,33 @@ describe("POST /auth/register", () => {
             };
 
             // Act
-            const response = await request(app).post("/auth/register");
+            const response = await request(app)
+                .post("/auth/register")
+                .send(user);
 
             // Assert
             expect(
                 (response.headers as Record<string, string>)["content-type"],
             ).toEqual(expect.stringContaining("json"));
+        });
+
+        it("should persist user in database", async () => {
+            // Arrange
+            const user = {
+                firstName: "Shravan",
+                lastName: "Chaudhary",
+                email: "shravan@gmail.com",
+                password: "password",
+            };
+
+            // Act
+            await request(app).post("/auth/register").send(user);
+
+            // Assert
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+
+            expect(users).toHaveLength(1);
         });
     });
 
