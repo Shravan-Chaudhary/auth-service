6768808a22931af577aa4d9371c9a63a688760ba
Shravan-Chaudhary <shravansinghchaudhary@gmail.com>
2024-05-29 09:54:16 +0530

    feat: add User entity and update data source configuration

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,14 +1,15 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { User } from "./entity/User";
+import { User } from "../entity/User";
+import { Config } from ".";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
+    host: Config.DATABASE_HOST,
+    port: Number(Config.DATABASE_PORT),
+    username: Config.DATABASE_USERNAME,
+    password: Config.DATABASE_PASSWORD,
+    database: Config.DATABASE_NAME,
     synchronize: true,
     logging: false,
     entities: [User],
