45847e80e9bdba72faa1714a913590514b6be086
Shravan-Chaudhary <shravansinghchaudhary@gmail.com>
2024-05-30 10:00:56 +0530

    feat: enable json parsing in express middleware

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import authRouter from "./routes/auth";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 
 const app = express();
+// TODO: cookie parser
+
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
