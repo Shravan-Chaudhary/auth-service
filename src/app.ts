44caa9b0185a2e271728de773441b9270cf92ec8
Shravan-Chaudhary <shravansinghchaudhary@gmail.com>
2024-06-20 09:52:13 +0530

    fix: :bug: fix bcrypt binding issue with bcryptjs

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from "express";
 import authRouter from "./Auth/authRoute";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 
+// TODO: fix bcrypt with bcryptjs
+// TODO: docker compose notes
 const app = express();
 
 app.use(express.json());
