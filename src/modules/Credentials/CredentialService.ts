ae631d67dfb0b63793f8ba1b9be233fa17263016
Shravan-Chaudhary <shravansinghchaudhary@gmail.com>
2024-07-21 01:15:37 +0530

    feat: :sparkles: add CredentialService class for password hashing and comparison

---
+++ src/modules/Credentials/CredentialService.ts
@@ -0,0 +1,19 @@
+import bcrypt from "bcryptjs";
+import { createUnauthorizedError } from "../../common/errors/http-exceptions";
+
+export class CredentialService {
+    async hashPassword(password: string) {
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        return hashedPassword;
+    }
+
+    async comparePassword(password: string, hashedPassword: string) {
+        const match = await bcrypt.compare(password, hashedPassword);
+        if (!match) {
+            throw createUnauthorizedError("email or password does not match");
+        }
+        return match;
+    }
+}
