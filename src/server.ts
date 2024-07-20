2aa9737767b44bbcf50e8d38f9b37bd04f6d5c8b
Shravan-Chaudhary <shravansinghchaudhary@gmail.com>
2024-07-20 21:33:39 +0530

    feat: :sparkles: initialize database on server start

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import app from "./app";
 import { Config } from "./config";
+import AppDataSource from "./config/data-source";
 import logger from "./config/logger";
 
-const startServer = () => {
+const startServer = async () => {
     const PORT = Config.PORT ?? 5502;
     try {
+        await AppDataSource.initialize();
+        logger.info("Database initialized...");
+
         app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
     } catch (err: unknown) {
         if (err instanceof Error) {
@@ -16,4 +20,4 @@ const startServer = () => {
     }
 };
 
-startServer();
+void startServer();
