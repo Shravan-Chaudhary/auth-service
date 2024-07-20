1713ab6cdfbbf1e06948b1f83c0c9b63201a871c
Shravan-Chaudhary <shravansinghchaudhary@gmail.com>
2024-07-21 01:14:36 +0530

    add roles constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,9 @@
+export const Roles = {
+    CUSTOMER: "customer",
+    ADMIN: "admin",
+    MANAGER: "manager",
+};
+
 export const ONE_HOUR = 60 * 60 * 1000;
 export const ONE_DAY = 24 * ONE_HOUR;
 export const ONE_YEAR = 365 * ONE_DAY;
