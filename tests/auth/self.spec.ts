f49b22956df52c95ec3d383dbf42e8ac57e9e9ed
Shravan-Chaudhary <shravansinghchaudhary@gmail.com>
2024-07-23 04:55:45 +0530

    test: :test_tube: test for user data on self endpoint

--- tests/auth/self.spec.ts
+++ tests/auth/self.spec.ts
@@ -2,20 +2,29 @@ import { DataSource } from "typeorm";
 import AppDataSourse from "../../src/config/data-source";
 import request from "supertest";
 import app from "../../src/app";
+import { User } from "../../src/entity/User";
+import createJWKSMock, { JWKSMock } from "mock-jwks";
+import { Roles } from "../../src/constants";
 
 describe("GET /auth/self", () => {
     let connection: DataSource;
+    let jwks: JWKSMock;
     const URL = "/api/v1/auth/self";
 
     beforeAll(async () => {
-        // Initialize the database connection
+        jwks = createJWKSMock("http://localhost:5501");
         connection = await AppDataSourse.initialize();
     });
 
     beforeEach(async () => {
+        jwks.start();
         // Clear the database before each test
         await connection.dropDatabase();
         await connection.synchronize();
+    });
+
+    afterEach(async () => {
+        jwks.stop();
     });
 
     afterAll(async () => {
@@ -30,6 +39,30 @@ describe("GET /auth/self", () => {
             expect(response.status).toBe(200);
         });
 
-        it("should return user data json", async () => {});
+        it("should return user data json", async () => {
+            // Arrange
+            const userData = {
+                firstName: "Shravan",
+                lastName: "Chaudhary",
+                email: "shravan@gmail.com",
+                password: "password",
+                role: Roles.CUSTOMER,
+            };
+
+            const userRepository = connection.getRepository(User);
+            const user = await userRepository.save(userData);
+
+            // Act
+            const accessToken = jwks.token({
+                sub: String(user.id),
+                role: user.role,
+            });
+
+            const response = await request(app)
+                .get(URL)
+                .set("Cookie", [`accessToken=${accessToken}`]);
+            // Assert
+            expect((response.body as Record<string, string>).id).toBe(user.id);
+        });
     });
 });
