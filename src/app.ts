861a892727981b41452bdfa932b7393487de82c1
Shravan-Chaudhary <shravansinghchaudhary@gmail.com>
2024-07-23 12:36:51 +0530

    fix: :bug: add cookie parser as global middleware

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import "reflect-metadata";
 import express, { Request, Response } from "express";
 import authRouter from "./modules/Auth/authRoute";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
