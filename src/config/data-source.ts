96ef4891d707c162b24cadccd00947269da7f6ad
Shravan-Chaudhary <shravansinghchaudhary@gmail.com>
2024-08-06 23:37:01 +0530

    fix: :wrench: update data-source configuration

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -11,10 +11,10 @@ const AppDataSource = new DataSource({
     username: Config.DATABASE_USERNAME,
     password: Config.DATABASE_PASSWORD,
     database: Config.DATABASE_NAME,
-    synchronize: true,
+    synchronize: false,
     logging: false,
     entities: [User, RefreshToken],
-    migrations: [],
+    migrations: ["src/migrations/*.ts"],
     subscribers: [],
 });
 
