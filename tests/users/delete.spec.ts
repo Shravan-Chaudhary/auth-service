93640d44d53b4c24c762cc6fc92de6f77cecd95c
Shravan-Chaudhary <shravansinghchaudhary@gmail.com>
2024-09-07 17:43:57 +0530

    test: :test_tube: Add test to delete user from database

--- tests/users/delete.spec.ts
+++ tests/users/delete.spec.ts
@@ -33,6 +33,7 @@ describe("DELETE /users/:id", () => {
     // Happy Path
     describe("Given all fields", () => {
         it("should return 204 status code", async () => {
+            // Arrange
             const userData = {
                 firstName: "Shravan",
                 lastName: "Chaudhary",
@@ -59,7 +60,34 @@ describe("DELETE /users/:id", () => {
             expect(response.statusCode).toBe(204);
         });
 
-        it.todo("should delete the user from database");
+        it("should delete the user from database", async () => {
+            // Arrange
+            const userData = {
+                firstName: "Shravan",
+                lastName: "Chaudhary",
+                email: "shravan@gmail.com",
+                password: "password",
+                tenantId: 1,
+                role: Roles.MANAGER,
+            };
+
+            const userRepository = connection.getRepository(User);
+            const savedUser = await userRepository.save(userData);
+
+            const adminToken = jwks.token({
+                sub: "1",
+                role: Roles.ADMIN,
+            });
+
+            // Act
+            await request(app)
+                .delete(`${URL}/${savedUser.id}`)
+                .set("Cookie", [`accessToken=${adminToken}`]);
+
+            // Assert
+            const users = await userRepository.find();
+            expect(users).toHaveLength(0);
+        });
     });
 
     // Sad Path
