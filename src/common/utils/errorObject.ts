388e8f9c5bfe0073afb0042d692c8a638df5ab89
Shravan <shravansinghchaudhary@gmail.com>
2024-10-14 13:41:47 +0530

    feat: Add createErrorObject utility function
    This commit adds the utility function createErrorObject to the src/common/utils/errorObject.ts file. The function is responsible for creating an error object based on the provided HttpError and Request objects. It sets the necessary properties such as success, statusCode, request details, message, data, and trace. Additionally, it logs the error using the logger module. In production mode, the IP and trace properties are removed from the error object.
    
    Co-authored-by: [Author Name]

---
+++ src/common/utils/errorObject.ts
@@ -0,0 +1,35 @@
+import { Request } from "express";
+import { HttpError } from "http-errors";
+import { EApplicationEnvoirnment } from "../constants/application";
+import { THttpError } from "../http/httpTypes";
+import logger from "../../config/logger";
+import { Config } from "../../config";
+
+const createErrorObject = (err: HttpError, req: Request): THttpError => {
+    const errorObject: THttpError = {
+        success: false,
+        statusCode: err.statusCode ?? err.status ?? 500,
+        request: {
+            ip: req.ip ?? null,
+            method: req.method,
+            url: req.originalUrl,
+        },
+        message: err.message,
+        data: null,
+        trace: { stack: err.stack },
+    };
+
+    logger.error("Controller_error", {
+        meta: errorObject,
+    });
+
+    // Remove IP,Trace if in production
+    if (Config.ENV === EApplicationEnvoirnment.PRODUCTION) {
+        delete errorObject.request.ip;
+        delete errorObject.trace;
+    }
+
+    return errorObject;
+};
+
+export default createErrorObject;
