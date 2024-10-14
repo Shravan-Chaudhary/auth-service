3f3ff71d0e4b56e826b2004158d9389059a2afdb
Shravan <shravansinghchaudhary@gmail.com>
2024-10-14 13:28:17 +0530

    chore: Update ESLint config to ignore unused variables

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,5 +18,17 @@ module.exports = {
         "@typescript-eslint/require-await": "off",
         "@typescript-eslint/no-misused-promises": "off",
         "@typescript-eslint/restrict-template-expressions": "off",
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            {
+                args: "all",
+                argsIgnorePattern: "^_",
+                caughtErrors: "all",
+                caughtErrorsIgnorePattern: "^_",
+                destructuredArrayIgnorePattern: "^_",
+                varsIgnorePattern: "^_",
+                ignoreRestSiblings: true,
+            },
+        ],
     },
 };
